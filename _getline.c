f20f053eeeea1f9ce85c99566675f89e597b253c
Olaitan34 <emmfatsneh@gmail.com>
2023-11-16 03:34:06 +0100

    revised

--- _getline.c
+++ _getline.c
@@ -12,37 +12,39 @@
 
 int check_logic_ops(char *array_commands[], int i, char array_operators[])
 {
-char *temp = NULL;
-int j;
+	char *temp = NULL;
+	int j;
 
-for (j = 0; array_commands[i] != NULL  && array_commands[i][j]; j++)
-{
-if (array_commands[i][j] == '&' && array_commands[i][j + 1] == '&')
-{
-/* this is to split the line when chars && is found */
-temp = array_commands[i];
-array_commands[i][j] = '\0';
-array_commands[i] = str_duplicate(array_commands[i]);
-array_commands[i + 1] = str_duplicate(temp + j + 2);
-i++;
-array_operators[i] = '&';
-free(temp);
-j = 0;
-}
-if (array_commands[i][j] == '|' && array_commands[i][j + 1] == '|')
-{
-/*block to split the line when chars || was found */
-temp = array_commands[i];
-array_commands[i][j] = '\0';
-array_commands[i] = str_duplicate(array_commands[i]);
-array_commands[i + 1] = str_duplicate(temp + j + 2);
-i++;
-array_operators[i] = '|';
-free(temp);
-j = 0;
-}
-}
-return (i);
+	for (j = 0; array_commands[i] != NULL  && array_commands[i][j]; j++)
+	{
+		if (array_commands[i][j] == '&' && array_commands[i][j + 1] == '&')
+		{
+
+			/* this is to split the line when chars && is found */
+			temp = array_commands[i];
+			array_commands[i][j] = '\0';
+			array_commands[i] = str_duplicate(array_commands[i]);
+			array_commands[i + 1] = str_duplicate(temp + j + 2);
+			i++;
+			array_operators[i] = '&';
+			free(temp);
+			j = 0;
+		}
+
+		if (array_commands[i][j] == '|' && array_commands[i][j + 1] == '|')
+		{
+			/*block to split the line when chars || was found */
+			temp = array_commands[i];
+			array_commands[i][j] = '\0';
+			array_commands[i] = str_duplicate(array_commands[i]);
+			array_commands[i + 1] = str_duplicate(temp + j + 2);
+			i++;
+			array_operators[i] = '|';
+			free(temp);
+			j = 0;
+		}
+	}	
+	return (i);
 }
 
 
