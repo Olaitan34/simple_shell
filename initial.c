6e8660d4d6f8c2afa0f29abb104ae06ba2434130
Olaitan34 <emmfatsneh@gmail.com>
2023-11-16 03:01:24 +0100

    revised

---
+++ initial.c
@@ -0,0 +1,65 @@
+#include "shell.h"
+
+/**
+ * handle_ctrl_c - Funvtion that prints the prompt in a new line
+ * @UNUSED: The option of the prototype, itbis an integer variable
+ */
+
+void handle_ctrl_c(int opr UNUSED)
+{
+	_print("\n");
+	_print("$ ");
+}
+
+
+/**
+ * initialize_data - Function that initializes the structs
+ * @data: variable which is a pointer to the struct of data
+ * @argv: variable which is an array of arg passed to the program execution
+ * @env: variable which is environ passed to the program execution
+ * @argc: this is the num of values received from the command line
+ */
+
+void initialize_data(data_of_program *data, int argc, char *argv[], char **env)
+{
+	int i = 0;
+
+	data->program_name = argv[0];
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->exec_counter = 0;
+	
+	if (argc == 1)
+	{
+		data->file_descriptor = STDIN_FILENO;
+	}
+	else
+	{
+		data->file_descriptor = open(argv[1], O_RDONLY);
+		if (data->file_descriptor == -1)
+		{
+			_printe(data->program_name);
+			_printe(": 0: Can't open ");
+			_printe(argv[1]);
+			_printe("\n");
+			exit(127);
+		}
+	}
+	data->tokens = NULL;
+	data->env = malloc(sizeof(char *) * 50);
+	if (env)
+	{
+		for (; env[i]; i++)
+		{
+			data->env[i] = str_duplicate(env[i]);
+		}
+	}
+	data->env[i] = NULL;
+	env = data->env;
+
+	data->alias_list = malloc(sizeof(char *) * 20);
+	for (i = 0; i < 20; i++)
+	{
+		data->alias_list[i] = NULL;
+	}
+}
---
+++ sisifo.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+/**
+ * sisifo - Function which is an infinite loop that shows the prompt
+ * @prompt: variable carrying the prompt to be printed
+ * @data: variable carrying an infinite loop that shows the prompt
+ */
+
+void sisifo(data_of_program *data)
+{
+	int error = 0;
+	int len = 0;
+
+	while (++(data->exec_counter))
+	{
+		_print("$");
+
+		error = len = _getline(data);
+
+		if (error == EOF)
+		{
+			free_all_data(data);
+			exit(errno); 
+		}
+		if (len >= 1)
+		{
+			expand_alias(data);
+			expand_variables(data);
+			tokenize(data);
+			if (data->tokens[0])
+			{ 
+				error = execute(data);
+				if (error != 0)
+					_print_error(error, data);
+			}
+			free_recurrent_data(data);
+		}
+	}
+}
